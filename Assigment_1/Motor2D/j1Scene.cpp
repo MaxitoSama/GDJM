5734dc3abb870f92699873c61dc09d270564d1c1
joseppi <josep.pi.serra@gmail.com>
2017-11-15 16:14:06 +0100

    Scene 1 commented troublesome zombie

--- Assigment_1/Motor2D/j1Scene.cpp
+++ Assigment_1/Motor2D/j1Scene.cpp
@@ -47,7 +47,7 @@ bool j1Scene::Start()
 
 		//enemies
 		App->enemies->AddEnemy(ZOMBIE, 1500, 0);
-		App->enemies->AddEnemy(ZOMBIE, 11398, 245); 
+		//App->enemies->AddEnemy(ZOMBIE, 11398, 245); 
 		App->colliders->AddCollider({ 32,600,32,1 }, COLLIDER_FLOOR, this);
 		App->colliders->AddCollider({ 64,600,32,1 }, COLLIDER_FLOOR, this);
 		App->enemies->AddEnemy(PLANE, 1500, 100);
