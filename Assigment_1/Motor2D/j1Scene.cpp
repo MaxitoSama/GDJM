97cd22ea70dd165095ac51174ca8029e5f479dfe
joseppi <josep.pi.serra@gmail.com>
2017-11-17 20:08:44 +0100

    moved the zombie which didn't allow to go through the spikes of the first map

--- Assigment_1/Motor2D/j1Scene.cpp
+++ Assigment_1/Motor2D/j1Scene.cpp
@@ -54,7 +54,7 @@ bool j1Scene::Start()
 		App->entities->AddEnemy(ZOMBIE, 2500, 600);
 		//App->entities->AddEnemy(ZOMBIE, 7000, 600);
 		//App->entities->AddEnemy(ZOMBIE, 1300, 600);
-		//App->entities->AddEnemy(ZOMBIE, 11398, 245);
+		//App->entities->AddEnemy(ZOMBIE, 9812, 668);
 		//App->colliders->AddCollider({ 32,600,32,1 }, COLLIDER_FLOOR, this);
 		//App->colliders->AddCollider({ 64,600,32,1 }, COLLIDER_FLOOR, this);
 		App->entities->AddEnemy(PLANE, 1500, 100);
