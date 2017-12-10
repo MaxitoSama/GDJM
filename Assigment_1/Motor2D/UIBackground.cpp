09b1b995cf52b1489700a751d62989d1cd3f52b8
joseppi <josep.pi.serra@gmail.com>
2017-12-10 18:48:07 +0100

    Background addapts to the resolution of the game

--- Assigment_1/Motor2D/UIBackground.cpp
+++ Assigment_1/Motor2D/UIBackground.cpp
@@ -2,6 +2,7 @@
 #include "j1Render.h"
 #include "j1Gui.h"
 #include "UIBackground.h"
+#include "j1Window.h"
 
 
 UIBackground::UIBackground(int x, int y, UIElementType type, j1Module* modul) :UIElements(x, y, type, modul)
@@ -14,5 +15,13 @@ UIBackground::~UIBackground()
 
 void UIBackground::Draw()
 {
-	App->render->Blit(App->gui->GetBackground(), position.x, position.y, {},1.1f);
+	//Adapts the background to the screen
+	h_background = 907;
+	while (h_background*scale <= App->win->screen_surface->h)
+	{
+		scale += 0.01f;
+	}
+
+	//prints background
+	App->render->Blit(App->gui->GetBackground(), position.x, position.y, {},scale);
 }
